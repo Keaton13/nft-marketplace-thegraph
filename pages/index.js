2731154e3f3c2cc821a120c24d9f80118aa18b32
Keaton Krieger <keatonkrieger@Keatons-MacBook-Pro.local>
2022-09-01 17:42:34 -0700

    Fixed buy now issue

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import GET_ACTIVE_ITEMS from "../constants/subgraphQueries"
 export default function Home() {
     const { isWeb3Enabled, chainId } = useMoralis()
     const chainString = chainId ? parseInt(chainId).toString() : "31337"
-    const marketplaceAddress = networkMapping[chainString].NftMarketplace.address
+    const marketplaceAddress = networkMapping[chainString].NftMarketplace[0]
 
     const { loading, error, data: listedNfts } = useQuery(GET_ACTIVE_ITEMS)
 
